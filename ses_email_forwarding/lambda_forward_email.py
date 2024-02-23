feeef94737166192438c59c542af478a1ae267c9
Taylor Jacovich <tejacovich@gmail.com>
2024-02-23 20:44:43 +0000

    Initial setup of tests with python actions.

--- ses_email_forwarding/lambda_forward_email.py
+++ ses_email_forwarding/lambda_forward_email.py
@@ -35,6 +35,42 @@ def process_message(record):
         print("An error occurred")
         raise e
 
+def modify_email_message(raw_email):
+    """
+    params: 
+        string raw_email: The raw email from the S3 bucket
+
+    returns: 
+        string message: The modified email message
+        string from_addr: The new sender email
+        string to_addr: The address the email is being forwarded to
+
+    Modifies the email to use SES SMTP with user defined sender and recipient.
+    """
+    # create a Message instance from the email string
+    message = email.message_from_string(raw_email)
+    
+    #save the original sender for the Reply-to field
+    reply_to = message["From"]
+
+    #Get sending address (from_addr) and receiving address (to_addr) from environment
+    from_addr = os.environ['from_addr']
+    to_addr = os.environ['to_addr']
+
+    #Update from address to retain original sender name if possible
+    from_addr = str(reply_to.split("<")[0][:-1])+" <"+str(from_addr)+">"
+
+    #Update email header fields
+    message.replace_header("To", to_addr)
+    message.replace_header("From", from_addr)
+    message.replace_header("Return-Path", from_addr)
+
+    #Add Sender and reply-to header fields
+    message.add_header("Sender", from_addr)
+    message.add_header('reply-to', reply_to)
+
+    return message, from_addr, to_addr
+
 def forward_ses_email(s3_key):
     """
     params: 
@@ -64,28 +100,8 @@ def forward_ses_email(s3_key):
 
     #read email from s3 bucket and store it as utf-8 string.
     raw_email=s3_client.get_object(Bucket=bucket, Key=s3_key)['Body'].read().decode('utf-8')
-    
-    # create a Message instance from the email string
-    message = email.message_from_string(raw_email)
-    
-    #save the original sender for the Reply-to field
-    reply_to = message["From"]
 
-    #Get sending address (from_addr) and receiving address (to_addr) from environment
-    from_addr = os.environ['from_addr']
-    to_addr = os.environ['to_addr']
-
-    #Update from address to retain original sender name if possible
-    from_addr = str(reply_to.split("<")[0][:-1])+" <"+str(from_addr)+">"
-
-    #Update email header fields
-    message.replace_header("To", to_addr)
-    message.replace_header("From", from_addr)
-    message.replace_header("Return-Path", from_addr)
-
-    #Add Sender and reply-to header fields
-    message.add_header("Sender", from_addr)
-    message.add_header('reply-to', reply_to)
+    message, from_addr, to_addr = modify_email_message(raw_email)
 
     #SMTP credentials
     smtp_host = os.environ['smtp_server']
